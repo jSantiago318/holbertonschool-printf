8ab545564cd750f92e29d6135ddacf5f033bbc8e
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-21 19:21:43 -0400

    compiler problem

--- _printf.c
+++ _printf.c
@@ -38,8 +38,7 @@ int _printf(const char *format, ...)
       if (*ptr == '\0')
       {
         /*si no hay mas codigo o string para leer para de correr*/
-        _putchar('%');
-        count++;
+        
         break;
       }
       else if (*ptr == 'c')
