0eae263787a2b31472d00936c3b8911c35912ffc
Joshua Santiago <joshuasantiago111@gmail.com>
2026-03-21 11:50:10 -0400

    syntax bug

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+ 
 #include "main.h"
 #include <string.h>
 
@@ -11,7 +12,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count;
-   
+  // string return_string
+  
 	const char *ptr;
 
 	if (format == NULL)
@@ -29,67 +31,37 @@ int _printf(const char *format, ...)
     {
       _putchar(*ptr);
       count++;
-    }else if (*ptr == '%') {
+    } else if (*ptr == '%') {
       ptr++;
 
-      /* 
-      * verify length of scring, if it is only % and 1 char long, return character
-      */
- 
-
+      /* if there is no more characters */
+      if (*ptr == '\0' && count == 0)
+      {
+          count += manager_percent(args);
+      }
 
       if (*ptr == 'r')
       {
-        return (-1);
+        return (-1); 
       }
       
-
-      /*
-       * %c print a single character
-       * /d print a decimal (base 10) number
-       * /e print an exponential floating-point number
-       * /f print a floating-point number
-       * /g print a general-format floating-point number
-       * /i print an integer in base 10
-       * /o print a number in octal (base 8)
-       * /s print a string of characters
-       * /u print an unsigned decimal (base 10) number
-       * /x print a number in hexidecimal (base 16)
-       * %% print a percent sign (\% also works)
-       */
-      
-       /*
-      if (*ptr == 'c')
-        count += manager_char( format);
-      else if (*ptr == 's')
-        count += manager_string( format);
-      else if (*ptr == '%') 
-      {
-        _putchar('%');
-        count++;
-      }
-        */
 
 
           /* VERIFICAR %c*/
         if (*ptr == 'c')
           count += manager_char(args);
-        /* VERIFICAR %s para string >> nested loop*/
+                  /* VERIFICAR %s para string >> nested loop*/
         if (*ptr == 's')
           count += manager_string(args);
-        /* VERIFICAR %d para decimal*/
+                  /* VERIFICAR %d para decimal*/
         if (*ptr == 'd')
           count += manager_int(args);
-        /* VERIFICAR %i para integer*/
+                  /* VERIFICAR %i para integer*/
         if (*ptr == 'i')
           count += manager_int(args);
-        /* VERIFICAR %% para percent sign*/
-        
+                  /* VERIFICAR %% para percent sign*/
         if (*ptr == '%')
-        {
-          _putchar('%');
-          count++;
-        }
+          count += manager_percent(args);
 
     }
 		ptr++;
